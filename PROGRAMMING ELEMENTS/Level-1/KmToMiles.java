2d8836b192a26b84c150b03745c1ec6fd33fdc3a
Shashank Sharma <shashanksharma240602005@gmail.com>
2025-08-17 00:47:02 +0530

    Added Core-Java folder

--- PROGRAMMING ELEMENTS/Level-1/KmToMiles.java
+++
@@ -1,8 +0,0 @@
-class KmToMiles {
-    public static void main(String[] args) {
-        double kilometers = 10.8;
-        double milesPerKm = 1.6;
-        double miles = kilometers * milesPerKm;
-        System.out.println("The distance " + kilometers + " km in miles is " + miles);
-    }
-}
