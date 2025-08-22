f0178c8cddbc70c326a5523972c09c4d89928d6b
Shashank Sharma <shashanksharma240602005@gmail.com>
2025-08-23 00:47:01 +0530

    NullPointerDemo

---
+++ Java String Practices/Level 1/NullPointerDemo.java
@@ -0,0 +1,17 @@
+class NullPointerDemo {
+    static void generate() {
+        String text=null;
+        System.out.println(text.length());
+    }
+    static void handle() {
+        try {
+            String text=null;
+            System.out.println(text.length());
+        } catch(NullPointerException e) {
+            System.out.println("Handled NullPointerException");
+        }
+    }
+    public static void main(String[] args) {
+        handle();
+    }
+}
