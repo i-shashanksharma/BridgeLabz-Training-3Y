2338c8ec9d2647568e088c3553b5a90494277e65
Shashank Sharma <shashanksharma240602005@gmail.com>
2025-08-23 01:57:37 +0530

    UniqueCharacters

---
+++ Java String Practices/Level 3/UniqueCharacters.java
@@ -0,0 +1,49 @@
+import java.util.*;
+class UniqueCharacters {
+    static int getLength(String text) {
+        int count = 0;
+        try {
+            while (true) {
+                text.charAt(count);
+                count++;
+            }
+        } catch (Exception e) {}
+        return count;
+    }
+
+    static char[] findUnique(String text) {
+        int n = getLength(text);
+        char[] temp = new char[n];
+        int uniqueCount = 0;
+        for (int i = 0; i < n; i++) {
+            char ch = text.charAt(i);
+            boolean isUnique = true;
+            for (int j = 0; j < i; j++) {
+                if (ch == text.charAt(j)) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) {
+                temp[uniqueCount] = ch;
+                uniqueCount++;
+            }
+        }
+        char[] result = new char[uniqueCount];
+        for (int i = 0; i < uniqueCount; i++) {
+            result[i] = temp[i];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String text = sc.nextLine();
+        char[] unique = findUnique(text);
+        System.out.print("Unique characters: ");
+        for (char c : unique) {
+            System.out.print(c + " ");
+        }
+    }
+}
