941624f8c4e9a95d2eeaabd692799274cd9692f4
Shashank Sharma <shashanksharma240602005@gmail.com>
2025-09-04 01:30:38 +0530

    [Shashank] add. BMICalculator

---
+++ Java Methods/Level 2/BMICalculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class BMICalculator {
+    public static double calculateBMI(double weight, double heightCm) {
+        double heightM = heightCm / 100;
+        return weight / (heightM * heightM);
+    }
+
+    public static String getStatus(double bmi) {
+        if (bmi <= 18.4) return "Underweight";
+        else if (bmi <= 24.9) return "Normal";
+        else if (bmi <= 39.9) return "Overweight";
+        else return "Obese";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[][] data = new double[10][3];
+        String[] status = new String[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter weight (kg) of person " + (i+1) + ": ");
+            data[i][0] = sc.nextDouble();
+            System.out.print("Enter height (cm) of person " + (i+1) + ": ");
+            data[i][1] = sc.nextDouble();
+            data[i][2] = calculateBMI(data[i][0], data[i][1]);
+            status[i] = getStatus(data[i][2]);
+        }
+        System.out.println("\nWeight(kg)\tHeight(cm)\tBMI\t\tStatus");
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%.2f\t\t%.2f\t\t%.2f\t%s\n", data[i][0], data[i][1], data[i][2], status[i]);
+        }
+    }
+}
