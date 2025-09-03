5415e8a8bb49c535d8efb7238db3caf051a4eef7
Shashank Sharma <shashanksharma240602005@gmail.com>
2025-09-04 01:27:29 +0530

    [Shashank] add. Number Checker

---
+++ Java Methods/Level 2/NumberCheck.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class NumberCheck {
+    public static boolean isPositive(int num) {
+        return num >= 0;
+    }
+
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    public static int compare(int num1, int num2) {
+        if (num1 > num2) return 1;
+        else if (num1 == num2) return 0;
+        else return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        System.out.println("Enter 5 numbers:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!isPositive(arr[i])) {
+                System.out.println(arr[i] + " is Negative");
+            } else {
+                if (isEven(arr[i])) {
+                    System.out.println(arr[i] + " is Positive and Even");
+                } else {
+                    System.out.println(arr[i] + " is Positive and Odd");
+                }
+            }
+        }
+        
+        int result = compare(arr[0], arr[arr.length - 1]);
+        if (result == 1) {
+            System.out.println("First element is greater than last element");
+        } else if (result == 0) {
+            System.out.println("First element is equal to last element");
+        } else {
+            System.out.println("First element is less than last element");
+        }
+    }
+}
