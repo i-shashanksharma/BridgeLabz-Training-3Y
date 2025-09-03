5d2eccdee2df0e5e72b6a4c5f7f84b6c9d078986
Shashank Sharma <shashanksharma240602005@gmail.com>
2025-09-03 16:43:44 +0530

    [Shashank] add. Trigonometric Function

---
+++ Java Methods/Level 1/TrigonometricFunctions.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class TrigonometricFunctions {
+    public static double[] calculateTrigonometricFunctions(double angle) {
+        double rad = Math.toRadians(angle);
+        return new double[]{Math.sin(rad), Math.cos(rad), Math.tan(rad)};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter angle in degrees:");
+        double angle = sc.nextDouble();
+
+        double[] result = calculateTrigonometricFunctions(angle);
+        System.out.println("Sine: " + result[0] + " Cosine: " + result[1] + " Tangent: " + result[2]);
+    }
+}
