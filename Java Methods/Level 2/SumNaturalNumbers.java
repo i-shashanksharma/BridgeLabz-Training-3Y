9a5e745fb4a4d162cec3e74a08c5afb16cc07538
Shashank Sharma <shashanksharma240602005@gmail.com>
2025-09-03 17:08:51 +0530

    [Shashank] add. Sum of numbers

---
+++ Java Methods/Level 2/SumNaturalNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class SumNaturalNumbers {
+    public static int sumRecursive(int n) {
+        if (n == 0) return 0;
+        return n + sumRecursive(n - 1);
+    }
+
+    public static int sumFormula(int n) {
+        return n * (n + 1) / 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a natural number:");
+        int n = sc.nextInt();
+        int sum1 = sumRecursive(n);
+        int sum2 = sumFormula(n);
+        System.out.println("Recursive Sum = " + sum1);
+        System.out.println("Formula Sum = " + sum2);
+    }
+}
