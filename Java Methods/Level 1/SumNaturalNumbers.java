07e61afc357a53fd146abd9488aa226bbfe7ceda
Shashank Sharma <shashanksharma240602005@gmail.com>
2025-09-03 16:42:36 +0530

    [Shashank] add. Sum of natural numbers

---
+++ Java Methods/Level 1/SumNaturalNumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class SumNaturalNumbers {
+    public static int calculateSum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) sum += i;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n:");
+        int n = sc.nextInt();
+        System.out.println("Sum: " + calculateSum(n));
+    }
+}
