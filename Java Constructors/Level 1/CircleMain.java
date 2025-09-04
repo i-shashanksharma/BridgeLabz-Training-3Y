cb94fdd0787a577d679dc30d4c1edafa96ebcb84
Shashank Sharma <shashanksharma240602005@gmail.com>
2025-09-04 15:00:48 +0530

    [Shashank] add. Circle radius

---
+++ Java Constructors/Level 1/CircleMain.java
@@ -0,0 +1,9 @@
+public class CircleMain {
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+        Circle c2 = new Circle(5.5);
+
+        System.out.println("Default radius: " + c1.radius);
+        System.out.println("Custom radius: " + c2.radius);
+    }
+}
