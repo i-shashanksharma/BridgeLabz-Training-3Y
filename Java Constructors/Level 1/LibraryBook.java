320daca4d46c4ba34cbbbba0b8776f3d1f9f65d8
Shashank Sharma <shashanksharma240602005@gmail.com>
2025-09-04 15:39:14 +0530

    [Shashank] add. Library Book

---
+++ Java Constructors/Level 1/LibraryBook.java
@@ -0,0 +1,22 @@
+public class LibraryBook {
+    String title;
+    String author;
+    double price;
+    boolean available;
+
+    public LibraryBook(String title, String author, double price, boolean available) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.available = available;
+    }
+
+    public void borrowBook() {
+        if (available) {
+            available = false;
+            System.out.println(title + " borrowed successfully.");
+        } else {
+            System.out.println(title + " is not available.");
+        }
+    }
+}
