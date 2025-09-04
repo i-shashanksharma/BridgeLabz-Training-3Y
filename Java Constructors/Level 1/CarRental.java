2028c9191e099391e549161ba55d973b3ce38048
Shashank Sharma <shashanksharma240602005@gmail.com>
2025-09-04 15:41:25 +0530

    [Shashank] add. Car rental

---
+++ Java Constructors/Level 1/CarRental.java
@@ -0,0 +1,22 @@
+public class CarRental {
+    String customerName;
+    String carModel;
+    int rentalDays;
+    double costPerDay = 1000.0;
+
+    public CarRental() {
+        this.customerName = "Unknown";
+        this.carModel = "Standard";
+        this.rentalDays = 1;
+    }
+
+    public CarRental(String customerName, String carModel, int rentalDays) {
+        this.customerName = customerName;
+        this.carModel = carModel;
+        this.rentalDays = rentalDays;
+    }
+
+    public double calculateTotalCost() {
+        return rentalDays * costPerDay;
+    }
+}
