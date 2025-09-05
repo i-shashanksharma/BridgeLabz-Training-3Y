5ce1471b785b9d37a3dad06065e0f4c2a93dee81
Shashank Sharma <shashanksharma240602005@gmail.com>
2025-09-05 11:20:02 +0530

    [Shashank] add.  Football Team Details

---
+++ Java Methods/Level 3/FootballTeam.java
@@ -0,0 +1,59 @@
+import java.util.Random;
+public class FootballTeam {
+    public static void main(String[] args) {
+        int[] heights = new int[11];
+        Random rand = new Random();
+
+        for (int i = 0; i < heights.length; i++) {
+            heights[i] = 150 + rand.nextInt(101);
+        }
+
+        System.out.print("Heights of players: ");
+        for (int h : heights) {
+            System.out.print(h + " ");
+        }
+        System.out.println();
+
+        int sum = findSum(heights);
+        double mean = findMean(heights);
+        int shortest = findShortest(heights);
+        int tallest = findTallest(heights);
+
+        System.out.println("Sum of all heights: " + sum);
+        System.out.println("Mean height: " + mean);
+        System.out.println("Shortest height: " + shortest);
+        System.out.println("Tallest height: " + tallest);
+    }
+
+    public static int findSum(int[] arr) {
+        int sum = 0;
+        for (int val : arr) {
+            sum += val;
+        }
+        return sum;
+    }
+
+    public static double findMean(int[] arr) {
+        return (double) findSum(arr) / arr.length;
+    }
+
+    public static int findShortest(int[] arr) {
+        int min = arr[0];
+        for (int val : arr) {
+            if (val < min) {
+                min = val;
+            }
+        }
+        return min;
+    }
+
+    public static int findTallest(int[] arr) {
+        int max = arr[0];
+        for (int val : arr) {
+            if (val > max) {
+                max = val;
+            }
+        }
+        return max;
+    }
+}
